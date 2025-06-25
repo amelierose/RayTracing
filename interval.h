68348b669f212a466fbae675997971dba71931b8
Amelie Lundrigan <a.lundrigan@outlook.com>
2025-06-25 14:52:45 +0100

    Added surface norms, hittable obj list and utility header

---
+++ interval.h
@@ -0,0 +1,33 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+class interval {
+	public:
+	double min, max;
+	
+	//default interval
+	interval() : min(+infinity), max(-infinity) {}
+	
+	//defining interval
+	interval(double min, double max) : min(min), max(max) {}
+	
+	double size() const {
+		return max - min;
+	}
+	
+	bool contains(double x) const {
+		return min <= x && x <= max;
+	}
+	
+	bool surrounds(double x) const {
+		return min < x && x < max;
+	}
+	
+	static const interval empty, universe;
+	
+};
+
+const interval interval::empty    = interval(+infinity, -infinity);
+const interval interval::universe = interval(-infinity, +infinity);
+
+#endif
--- main.cc
+++ main.cc
@@ -1,34 +1,13 @@
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "rtweekend.h"
 
-#include <iostream>
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-//calculating whether the ray hits the sphere or not
-//this is done by finding solutions with the quadratic equation
-double hit_sphere(const point3& center, double radius, const ray& r) {
-	vec3 oc = center - r.origin();
-	auto a = r.direction().length_squared();
-	auto h = dot(r.direction(), oc);
-	auto c = oc.length_squared() - radius * radius;
-	auto discriminant = h*h - a*c;
-	
-	if (discriminant < 0) {
-		return -1.0;
-	}
-	
-	//only normalise if the ray actually hits the sphere
-	else {
-		return (h - std::sqrt(discriminant)) / a;
-	}
-}
-
-//outputting xyz/rgb colour according to the surface normal
-color ray_color(const ray& r) {
-	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-	if (t > 0.0) {
-		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-		return 0.5 * color (N.x()+1, N.y()+1, N.z()+1);
+color ray_color(const ray& r, const hittable& world) {
+	hit_record rec;
+	if (world.hit(r, interval(0, infinity), rec)) {
+		return 0.5 * (rec.normal + color(1, 1, 1));
 	}
 	
 	vec3 unit_direction = unit_vector(r.direction());
@@ -45,6 +24,12 @@ int main() {
 	//calculate image height and make sure it's at least 1
 	int image_height = int(image_width / aspect_ratio);
 	image_height = (image_height < 1) ? 1 : image_height;
+	
+	//world
+	hittable_list world;
+	
+	world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+	world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 	
 	//camera
 	
@@ -78,7 +63,7 @@ int main() {
 			auto ray_direction = pixel_center - camera_center;
 			ray r(camera_center, ray_direction);
 			
-			color pixel_color = ray_color(r);
+			color pixel_color = ray_color(r, world);
 			write_color(std::cout, pixel_color);
 		}
 	}
