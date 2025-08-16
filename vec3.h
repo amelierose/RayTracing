50af47b05c9bf7e6bd94690883e4cd2e50653159
Amelie Lundrigan <a.lundrigan@outlook.com>
2025-08-16 22:46:17 +0100

    diffuse material (9.1)

--- vec3.h
+++ vec3.h
@@ -47,7 +47,17 @@ class vec3 {
 		//defining length squared function
 		double length_squared() const {
 			return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
-		}		
+		}
+
+		//returns random vec3
+		static vec3 random() {
+			return vec3(random_double(), random_double(), random_double());
+		}
+		
+		//returns random vec3 within min max parameters
+		static vec3 random(double min, double max) {
+			return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+		}
 };
 
 using point3 = vec3;
@@ -106,6 +116,25 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 //normalise length to a unit vector
 inline vec3 unit_vector(const vec3& v) {
 	return v / v.length();
+}
+	
+//generating a random unit vector within (-1,1)
+inline vec3 random_unit_vector() {
+	while (true) {
+		auto p = vec3::random(-1,1);
+		auto lensq = p.length_squared();
+		if (1e-160 < lensq && lensq <= 1)
+			return p / sqrt(lensq);
+	}
+}
+
+//calculate which hemisphere the vector is on
+inline vec3 random_on_hemisphere(const vec3& normal) {
+	vec3 on_unit_sphere = random_unit_vector();
+	if (dot(on_unit_sphere, normal) > 0.0)
+		return on_unit_sphere;
+	else
+		return -on_unit_sphere;
 }
 
 #endif
